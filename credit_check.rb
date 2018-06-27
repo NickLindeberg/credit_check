043831b736df78d92a41c6c26d4837e04ed8d9b4
Nick Lindeberg <lindebergnick@gmail.com>
2018-06-26 21:37:30 -0600

    first update

--- credit_check.rb
+++ credit_check.rb
@@ -1,11 +1,16 @@
 card_number = "4929735477250543"
-print card_number.to_i.digits
+
+print card_array = [card_number]
+
+#this section will have an arrary of some sort.
+#multiply every other element of the array by 2
+#if statement that adds says the sum of the two values is <10 then add them together
 
 
+sum_card = 
 
-
-if valid
+if sum_card % 10 = 0
   puts "The number is valid!"
 else
-  puts "The number is invalid"
+  puts "The number is invald"
 end
