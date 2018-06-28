18a6595ee0591bc8c2ee08da0fd1fd09203f3b6e
Nick Lindeberg <lindebergnick@gmail.com>
2018-06-27 18:14:26 -0600

    section1

--- credit_check.rb
+++ credit_check.rb
@@ -1,6 +1,48 @@
-card_number = "4929735477250543"
+card_number = ("5541808923795240")
 
-print card_array = [card_number]
+card_array = card_number.chars.reverse
+
+card_array_i = []
+
+  card_array.each do |number|
+    number.to_i
+    card_array_i << number.to_i
+  end
+
+array_doubled = []
+
+  card_array_i.each.with_index do | digit, index |
+    if index.odd?
+      digit = digit * 2
+      array_doubled << digit
+    else index.even?
+      array_doubled << digit
+    end
+  end
+
+sum_digits = []
+
+  array_doubled.each do | digit |
+    if digit > 9
+      digit = digit - 9
+      sum_digits << digit
+    else
+      sum_digits << digit
+    end
+  end
+
+sum_total = sum_digits.sum
+
+
+if sum_total % 10 == 0
+  puts "The number is valid"
+else
+  puts "The number is invalid"
+end
+
+
+
+
 
 #this section will have an arrary of some sort.
 #multiply every other element of the array by 2
@@ -9,11 +51,13 @@ print card_array = [card_number]
 
 
 
-card_total =
-sum_card = card_total.sum
-
-if sum_card % 10 = 0
-  puts "The number is valid!"
-else
-  puts "The number is invalid"
-end
+# card_total =
+# sum_card = card_total.sum
+#
+# #need to make
+#
+# if sum_card % 10 = 0
+#   puts "The number is valid!"
+# else
+#   puts "The number is invalid"
+# end
